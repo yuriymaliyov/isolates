b8e0fd8f5434098a8d7c14eed42f38390c01493f
yuriymaliyove <yuriy.maliyov@influur.com>
2024-11-14 20:55:00 +0300

    update app title for deployment

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Deployment',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
